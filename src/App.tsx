2352ae5d77f938323579c2d0ef84dc5a6eceb39b
PeanutEz <phamlac10@gmail.com>
2025-10-02 22:03:48 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HappyBirthday from './pages/HappyBirthday'
+
+function App() {
+
+  return (
+    <>
+      <HappyBirthday />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HappyBirthday.tsx
@@ -0,0 +1,13 @@
+import styles from './HappyBirthday.module.css';
+
+function HappyBirthday() {
+  return (
+    <div className={styles.happyBirthdayContainer}>
+        <div className={styles.header}>
+            <h1>Happy Birthday!</h1>
+        </div>
+    </div>
+  );
+}
+
+export default HappyBirthday;
